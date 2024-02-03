7cadf4ac4e742a3526139ccb924af8fdcf5767c4
Dewan Masuma Nazneen <masumanazneen2019@gmail.com>
2024-02-03 22:33:40 +0600

    infinity array solution

--- searchBinary/infiniteArr.js
+++ searchBinary/infiniteArr.js
@@ -2,10 +2,44 @@
 
 
 // solution by taking fixed length chunck
+// function main(arr, target) {
+//     let chunkstart = 0;
+//     let chunkend = 3;
+//     while (arr[chunkstart]) {
+//         if (!arr[chunkend]) {
+//             chunkend--;
+//             continue;
+//         }
+//         let start = chunkstart;
+//         let end = chunkend;
+//         console.log(start, end)
+//         while (start <= end) {
+//             const mid = Math.floor((start + end) / 2)
+//             if (arr[mid] === target) return mid;
+
+//             if (arr[mid] < target) {
+//                 start = mid + 1
+//             }
+//             if (arr[mid] > target) {
+//                 end = mid - 1
+//             }
+//         }
+//         chunkstart = chunkstart + 4
+//         chunkend = chunkend + 4;
+//     }
+//     return -1
+// }
+
+// console.log(main([1, 2, 3, 5, 6, 7, 8, 9, 11, 44, 21, 34, 50], 50))
+
+
+
+// solution by increasing array size exponentially.
 function main(arr, target) {
     let chunkstart = 0;
-    let chunkend = 3;
+    let chunkend = 1;
     while (arr[chunkstart]) {
+        console.log(chunkstart, chunkend)
         if (!arr[chunkend]) {
             chunkend--;
             continue;
@@ -24,8 +58,9 @@ function main(arr, target) {
                 end = mid - 1
             }
         }
-        chunkstart = chunkstart + 4
-        chunkend = chunkend + 4;
+        // let temp = chunkstart;
+        // chunkstart = chunkend + 1;
+        // chunkend = (chunkend - temp + 1) * 2;
     }
     return -1
 }
